6ffef751df4db6149229cf07285c64c328291f6a
FERNANDO KASPARY FINK <fernandokfink@gmail.com>
2025-11-04 13:30:41 -0300

    email verification

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import User
+from . import validators
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["cpf", "name", "email", "birth_date", "created_at"]
         read_only_fields = ["id", "created_at"]
+
+    def validate_email(self, email):
+        if validators.isEmailValid(email):
+            return email
+        else:
+            raise serializers.ValidationError("Este e-mail não está autorizado.")
+        
