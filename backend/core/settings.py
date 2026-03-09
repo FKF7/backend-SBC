23f5a4ebce5568b8b98aa3cceb7611821391064a
FKF7 <fernandokfink@gmail.com>
2026-03-09 13:45:41 -0300

    changed allowed origins to ENV file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -60,8 +60,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = [os.environ.get("FRONTEND_URL")]
+CSRF_TRUSTED_ORIGINS = [os.environ.get("FRONTEND_URL")]
 
 ROOT_URLCONF = 'core.urls'
 
